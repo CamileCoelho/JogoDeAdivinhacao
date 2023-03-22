b0c94585ce70b5da0875b979ab1dbae61ac8ac1e
Camile Coelho <camilecoelho23@gmail.com>
2023-03-22 16:16:31 -0300

    Refatoração.

--- JogoDeAdivinhação.ConsoleApp/Program.cs
+++ JogoDeAdivinhação.ConsoleApp/Program.cs
@@ -1,151 +1,201 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace JogoDeAdivinhação.ConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            int nivelDificuldade, numeroChutes, suaPontuacao = 1000;
-            Random random = new Random();
-            int numeroSecreto = random.Next(1, 21);
-            string saida = "";
-            do
-            {
-                Console.Clear();
+        static int i, nivelDificuldade, numeroTotalChutes, suaPontuacao = 1000;
 
+        static string saida = "";
+
+        static string chute;
+
+        static Random random = new Random();
+        static int numeroSecreto = random.Next(1, 21);
+
+        static void MostrarTitulo()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("____________________________________________________________________");
+            Console.WriteLine();
+            Console.WriteLine("      Seja muito bem vindo ao jogo de adivinhação da Camile!");
+            Console.WriteLine("____________________________________________________________________");
+            Console.WriteLine();
+        }
+
+        static void EscolhaNivelDeDificuldade()
+        {
+            Console.WriteLine("      Escolha o nível de dificuldade do seu jogo:");
+            Console.WriteLine();
+            Console.WriteLine("      Fácil (1)      Médio (2)      Difícil (3)    ");
+            Console.WriteLine();
+            Console.Write("      = ");
+            nivelDificuldade = Convert.ToInt32(Console.ReadLine());
+
+            while (nivelDificuldade != 1 && nivelDificuldade != 2 && nivelDificuldade != 3)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("      Opção inexistente. Favor digitar um número de 1 a 3. ");
+                Console.WriteLine();
                 Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine("____________________________________________________________________");
-                Console.WriteLine();
-                Console.WriteLine("      Seja muito bem vindo ao jogo de adivinhação da Camile!");
-                Console.WriteLine("____________________________________________________________________");
-                Console.WriteLine();
-                Console.WriteLine("      Escolha o nível de dificuldade do seu jogo:");
-                Console.WriteLine();
-                Console.WriteLine("      Fácil (1)      Médio (2)      Difícil (3)    ");
-                Console.WriteLine();
                 Console.Write("      = ");
                 nivelDificuldade = Convert.ToInt32(Console.ReadLine());
-                
-                while (nivelDificuldade != 1 && nivelDificuldade != 2 && nivelDificuldade != 3)
+            }
+
+            if (nivelDificuldade == 1)
+                numeroTotalChutes = 15;
+            else if (nivelDificuldade == 2)
+                numeroTotalChutes = 10;
+            else
+                numeroTotalChutes = 5;
+        }
+
+        static void MostrarNomeDoJogo()
+        {
+            Console.WriteLine("____________________________________________________________________");
+            Console.WriteLine();
+            Console.WriteLine("      O jogo consiste em adivinhar um número aleatório entre 1 e 20. ");
+            Console.WriteLine("____________________________________________________________________");
+            Console.WriteLine();
+        }
+        static string ValoresChutes()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine();
+            Console.WriteLine("      Qual seu {0}º chute? ", i);
+            Console.WriteLine();
+            Console.Write("      = ");
+            string chute = Console.ReadLine();
+
+            return chute;
+        }        
+        static void ValidaçãoNumerosMenoresOuIguaisZero()
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("      Você não pode chutar zero ou números negativos. Tente novamente.");
+        }
+
+        static void LeituraQuantidadeChutes(int ordemChute)
+        {
+            Console.WriteLine();
+            Console.WriteLine("      Seu {0}º chute foi: {1} ", ordemChute, chute);
+        }
+
+        static void VisualizarMensagemValorPontuacao(string mensagem, ConsoleColor cor)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = cor;
+            Console.WriteLine(mensagem);
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("      Sua pontuação é de " + suaPontuacao);
+        }
+
+        static void VisualizarMensagemPerdeu()
+        {
+            Console.WriteLine("____________________________________________________________________");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("      Você perdeu :( ");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("      O número secreto era " + numeroSecreto);
+            Console.WriteLine("____________________________________________________________________");
+            Console.WriteLine();
+            Console.WriteLine("      Mas não desanime, jogue novamente!");
+        }
+        static void VisualizarMensagemAcertou()
+        {
+            Console.WriteLine();
+            Console.WriteLine("____________________________________________________________________");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("      Parabéns, você acertou! ");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("      O número secreto era " + numeroSecreto);
+            Console.WriteLine();
+            Console.WriteLine("      Sua pontuação foi de " + suaPontuacao);
+        }
+
+
+        static void PerguntarSeDesejaSairOuContinuarJoagndo()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("____________________________________________________________________");
+            Console.WriteLine();
+            Console.WriteLine("      Obrigada por jogar :) ");
+            Console.WriteLine("____________________________________________________________________");
+            Console.WriteLine();
+            Console.WriteLine("      Você deseja sair ou jogar outra vez? Digite \"SAIR\" \n      caso queira parar, ou qualquer tecla para continuar. ");
+            Console.WriteLine();
+            Console.Write("      ");
+            saida = Console.ReadLine().ToUpper();
+            Console.WriteLine("____________________________________________________________________");
+            Console.WriteLine();
+        }
+
+
+        static void Main(string[] args)
+        { 
+            do
+            {
+                random = new Random();
+
+                Console.Clear();
+
+                MostrarTitulo();
+
+                EscolhaNivelDeDificuldade();
+
+                MostrarNomeDoJogo();
+
+                for ( i = 1; i <= numeroTotalChutes; i++)
                 {
-                    Console.WriteLine();
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("      Opção inexistente. Favor digitar um número de 1 a 3. ");
-                    Console.WriteLine();
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.Write("      = ");
-                    nivelDificuldade = Convert.ToInt32(Console.ReadLine());
-                }
-
-                if (nivelDificuldade == 1)
-                    numeroChutes = 15;
-                else if (nivelDificuldade == 2)
-                    numeroChutes = 10;
-                else
-                    numeroChutes = 5;
-
-                Console.WriteLine("____________________________________________________________________");
-                Console.WriteLine();
-                Console.WriteLine("      O jogo consiste em adivinhar um número aleatório entre 1 e 20. ");
-                Console.WriteLine("____________________________________________________________________");
-                Console.WriteLine();
-
-                for ( int i = 1; i <= numeroChutes; i++)
-                {
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.WriteLine();
-                    Console.WriteLine("      Qual {0}º seu chute? ",i );
-                    Console.WriteLine();
-                    Console.Write("      = ");
-                    string chute = Console.ReadLine();
+                    chute = ValoresChutes();
                     int numeroChute = Convert.ToInt32(chute);
 
                     if (numeroChute <= 0)
                     {
-                        Console.WriteLine();
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.WriteLine("      Você não pode chutar zero ou números negativos. Tente novamente.");
+                        ValidaçãoNumerosMenoresOuIguaisZero();
                         i--;
                         continue;
                     }
+                    LeituraQuantidadeChutes(i);
 
-                    Console.WriteLine();
-                    Console.WriteLine("      Seu {0}º chute foi: {1} ",i ,chute);
-                    
                     bool acertou = numeroChute == numeroSecreto;
-                    bool perdeu = i == numeroChutes;
+                    bool perdeu = i == numeroTotalChutes;
                     bool maior = numeroChute > numeroSecreto;
-                    suaPontuacao = (suaPontuacao - Math.Abs((numeroChute - numeroSecreto) / 2)) ;
+                    suaPontuacao = (suaPontuacao - Math.Abs((numeroChute - numeroSecreto) / 2));
 
                     if (acertou)
                     {
-                        Console.WriteLine();
-                        Console.WriteLine("____________________________________________________________________");
-                        Console.WriteLine();
-                        Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        Console.WriteLine("      Parabéns, você acertou! ");
-                        Console.WriteLine();
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        Console.WriteLine("      O número secreto era " + numeroSecreto);
-                        Console.WriteLine();
-                        Console.WriteLine("      Sua pontuação foi de " + suaPontuacao);
+                        VisualizarMensagemAcertou();
                         break;
                     }
                     else
                     {
-
                         if (maior)
                         {
-                            Console.WriteLine();
-                            Console.ForegroundColor = ConsoleColor.DarkRed;
-                            Console.WriteLine("      Seu chute foi maior que o número secreto. Tente novamente. ");
-                            Console.WriteLine();
-                            Console.ForegroundColor = ConsoleColor.Magenta;
-                            Console.WriteLine("      Sua pontuação é de " + suaPontuacao);
+                            VisualizarMensagemValorPontuacao("      Seu chute foi maior que o número secreto. Tente novamente. ", ConsoleColor.DarkGray);
                         }
                         else
                         {
-                            Console.WriteLine();
-                            Console.ForegroundColor = ConsoleColor.DarkRed;
-                            Console.WriteLine("      Seu chute foi menor que o número secreto. Tente novamente. ");
-                            Console.WriteLine();
-                            Console.ForegroundColor = ConsoleColor.Magenta;
-                            Console.WriteLine("      Sua pontuação é de " + suaPontuacao);
+                            VisualizarMensagemValorPontuacao("      Seu chute foi menor que o número secreto. Tente novamente. ", ConsoleColor.DarkGray);
                         }
                     }
                     if (perdeu)
                     {
-                        Console.WriteLine("____________________________________________________________________");
-                        Console.WriteLine();
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.WriteLine("      Você perdeu :( ");
-                        Console.WriteLine();
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        Console.WriteLine("      O número secreto era " + numeroSecreto);
-                        Console.WriteLine();
-                        Console.WriteLine("      Sua pontuação foi de " + suaPontuacao);
-                        Console.WriteLine("____________________________________________________________________");
-                        Console.WriteLine();
-                        Console.WriteLine("      Mas não desanime, jogue novamente!");
+                        VisualizarMensagemPerdeu();
                         break;
                     }
                 }
 
-
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine("____________________________________________________________________");
-                Console.WriteLine();
-                Console.WriteLine("      Obrigada por jogar :) ");
-                Console.WriteLine("____________________________________________________________________");
-                Console.WriteLine();
-                Console.WriteLine("      Você deseja sair ou jogar outra vez? Digite \"SAIR\" \n      caso queira parar, ou qualquer tecla para continuar. ");
-                Console.WriteLine();
-                Console.Write("      ");
-                saida = Console.ReadLine().ToUpper();
-                Console.WriteLine("____________________________________________________________________");
-                Console.WriteLine();
-
+                PerguntarSeDesejaSairOuContinuarJoagndo();
+                
             } while (saida != "SAIR");
             Console.ForegroundColor = ConsoleColor.White;
 
